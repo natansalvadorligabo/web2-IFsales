665407af297e0a860e651dbc3756f3682682c00a
igorcardosoy-fedora <igorcardosoy@proton.me>
2024-11-25 21:49:01 -0300

    feat: class store, just to commit something

---
+++ src/main/java/br/com/ifsales/model/Store.java
@@ -0,0 +1,61 @@
+package br.com.ifsales.model;
+
+public class Store {
+    private Long id;
+    private String name;
+    private String cnjp;
+    private String region;
+    private String address;
+    private String phone;
+
+    public Store() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCnjp() {
+        return cnjp;
+    }
+
+    public void setCnjp(String cnjp) {
+        this.cnjp = cnjp;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
