8b73dd9caa451e26637ce116c75cb64b62cb5735
igorcardosoy-fedora <igorcardosoy@proton.me>
2024-11-28 16:31:49 -0300

    fix: user_id type

--- schema.sql
+++ schema.sql
@@ -4,7 +4,7 @@ USE ifsales;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
