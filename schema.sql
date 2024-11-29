d45b9e37115ccdb6c107b338ffbdf29549dc8ba1
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-11-29 02:51:11 -0300

    update schema sql

--- schema.sql
+++ schema.sql
@@ -9,4 +9,14 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS salesPersons;
+CREATE TABLE salesPersons (
+    salesPerson_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(16) NOT NULL,
+    active BOOL NOT NULL
+);
+
 select * from users;
+select * from salesPersons;
