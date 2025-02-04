bb789840461846a72f25e61c71458050cc0ad93f
giovanatrevizanbarbosa <gi.trevizan.barbosa@gmail.com>
2025-02-03 21:35:02 -0300

    feat(schema): added comments to schema and package spec and body

--- schema.sql
+++ schema.sql
@@ -1,3 +1,8 @@
+-- domínio: sistema de gerenciamento de vendas com usuários, vendedores, regiões, lojas, categorias, produtos e clientes.
+-- inclui um funil de vendas e um log de ações para rastrear mudanças.
+
+-- criação do usuário ifsales
+
 drop user ifsales cascade;
 
 create user ifsales identified by admin
@@ -9,47 +14,28 @@ grant create session, create table, create view
     , select any table, insert any table, drop any table
     , create public synonym to ifsales;
 
--- drop all tables and sequences if exist
+-- remoção de tabelas e sequências existentes
+
 begin
     for table_name in (
-        select table_name
-        from user_tables
-        where table_name in (
-                             'USERS'
-            ,'SALESPERSONS'
-            ,'REGIONS'
-            ,'STORES'
-            ,'CATEGORIES'
-            ,'CUSTOMERS'
-            ,'PRODUCTS'
-            ,'FUNNEL'
-            ,'ACTION_LOGS'
-            )
-        )
-        loop
-            execute immediate 'DROP TABLE ifsales.' || table_name.table_name || ' CASCADE CONSTRAINTS';
+        select table_name from user_tables
+        where table_name in ('USERS', 'SALESPERSONS', 'REGIONS', 'STORES', 'CATEGORIES',
+                             'CUSTOMERS', 'PRODUCTS', 'FUNNEL', 'ACTION_LOGS')
+        ) loop
+            execute immediate 'drop table ifsales.' || table_name.table_name || ' cascade constraints';
         end loop;
 
     for sequence_name in (
-        select sequence_name
-        from user_sequences
-        where sequence_name in (
-                                'USERS_SEQ'
-            ,'SALESPERSONS_SEQ'
-            ,'REGIONS_SEQ'
-            ,'STORES_SEQ'
-            ,'CATEGORIES_SEQ'
-            ,'PRODUCTS_SEQ'
-            ,'CUSTOMERS_SEQ'
-            ,'FUNNEL_SEQ'
-            ,'ACTION_LOGS_SEQ'
-            )
-        )
-        loop
-            execute immediate 'DROP SEQUENCE ifsales.' || sequence_name.sequence_name;
+        select sequence_name from user_sequences
+        where sequence_name in ('USERS_SEQ', 'SALESPERSONS_SEQ', 'REGIONS_SEQ', 'STORES_SEQ',
+                                'CATEGORIES_SEQ', 'PRODUCTS_SEQ', 'CUSTOMERS_SEQ', 'FUNNEL_SEQ', 'ACTION_LOGS_SEQ')
+        ) loop
+            execute immediate 'drop sequence ifsales.' || sequence_name.sequence_name;
         end loop;
 end;
 /
+
+-- criação de sequências para auto incremento
 
 create sequence ifsales.users_seq start with 1 increment by 1 nocycle nocache;
 create sequence ifsales.salespersons_seq start with 1 increment by 1 nocycle nocache;
@@ -60,6 +46,8 @@ create sequence ifsales.products_seq start with 1 increment by 1 nocycle nocache
 create sequence ifsales.customers_seq start with 1 increment by 1 nocycle nocache;
 create sequence ifsales.funnel_seq start with 1 increment by 1 nocycle nocache;
 create sequence ifsales.action_logs_seq start with 1 increment by 1 nocycle nocache;
+
+-- criação das tabelas
 
 create table ifsales.users (
      id int primary key
@@ -151,143 +139,7 @@ create table ifsales.action_logs (
     details varchar2(4000)
 );
 
-create or replace trigger ifsales.trg_log_categories
-    after insert or update or delete on ifsales.categories
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'CATEGORIES', 'INSERTED into CATEGORIES', current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'CATEGORIES', 'UPDATED CATEGORIES', current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'CATEGORIES', 'DELETED from CATEGORIES', current_timestamp, :old.id);
-    end if;
-end;
-/
-
-create or replace trigger ifsales.trg_log_customers
-    after insert or update or delete on ifsales.customers
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'CUSTOMERS', 'INSERTED into CUSTOMERS', current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'CUSTOMERS', 'UPDATED CUSTOMERS', current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'CUSTOMERS', 'DELETED from CUSTOMERS', user, current_timestamp, :old.id);
-    end if;
-end;
-/
-
-create or replace trigger ifsales.trg_log_products
-    after insert or update or delete on ifsales.products
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'PRODUCTS', 'INSERTED into PRODUCTS', current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'PRODUCTS', 'UPDATED PRODUCTS', current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'PRODUCTS', 'DELETED from PRODUCTS', current_timestamp, :old.id);
-    end if;
-end;
-/
-
-create or replace trigger ifsales.trg_log_regions
-    after insert or update or delete on ifsales.regions
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'REGIONS', 'INSERTED into REGIONS', current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'REGIONS', 'UPDATED REGIONS', current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'REGIONS', 'DELETED from REGIONS', current_timestamp, :old.id);
-    end if;
-end;
-/
-
-create or replace trigger ifsales.trg_log_salespersons
-    after insert or update or delete on ifsales.salespersons
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'SALESPERSONS', 'INSERTED into SALESPERSONS', current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'SALESPERSONS', 'UPDATED SALESPERSONS', current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'SALESPERSONS', 'DELETED from SALESPERSONS', current_timestamp, :old.id);
-    end if;
-end;
-/
-
-create or replace trigger ifsales.trg_log_stores
-    after insert or update or delete on ifsales.stores
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'STORES', 'INSERTED into STORES', current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'STORES', 'UPDATED STORES', current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'STORES', 'DELETED from STORES', current_timestamp, :old.id);
-    end if;
-end;
-/
-
-create or replace trigger ifsales.trg_log_users
-    after insert or update or delete on ifsales.users
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'USERS', 'INSERTED into USERS', current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'USERS', 'UPDATED USERS', current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'USERS', 'DELETED from USERS', current_timestamp, :old.id);
-    end if;
-end;
-
-create or replace trigger ifsales.trg_log_funnel
-    after insert or update or delete on ifsales.funnel
-    for each row
-begin
-    if inserting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'FUNNEL', 'INSERTED into FUNNEL'
-                , current_timestamp, :new.id);
-    elsif updating then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'FUNNEL', 'UPDATED FUNNEL'
-               ,current_timestamp, :new.id);
-    elsif deleting then
-        insert into ifsales.action_logs (id, table_name, action, action_date, details)
-        values (ifsales.action_logs_seq.nextval, 'FUNNEL', 'DELETED from FUNNEL'
-               ,current_timestamp, :old.id);
-    end if;
-end;
-/
+-- trigger para atualização da quantidade de vendas de produtos
 
 create or replace trigger ifsales.trg_update_product_sales
     after insert or update or delete on ifsales.funnel
@@ -298,13 +150,11 @@ begin
         set total_sales = total_sales - :old.product_quantity
         where id = :old.product_id;
     end if;
-
     if inserting then
         update ifsales.products
         set total_sales = total_sales + :new.product_quantity
         where id = :new.product_id;
     end if;
-
     if updating then
         update ifsales.products
         set total_sales = total_sales - :old.product_quantity + :new.product_quantity
@@ -312,6 +162,8 @@ begin
     end if;
 end;
 /
+
+-- inserção de dados iniciais
 
 insert into ifsales.salespersons values (ifsales.salespersons_seq.nextval, 'Caua Rufino', 'caua@gmail.com', '(16) 99633-7792', 1);
 insert into ifsales.salespersons values (ifsales.salespersons_seq.nextval, 'Nathan Ligabo', 'nathzinho@gmail.com', '(16) 99522-7194', 0);
